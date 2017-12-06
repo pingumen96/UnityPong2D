7bc8c9c9121b252929df4a94d4e6709a0e77fde6
pingumen96 <contatti@sinhuerossi.it>
2017-12-06 23:21:42 +0100

    Inizializzato repository

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(new Vector2(10.0f, 10.0f), ForceMode2D.Force);
+	}
+	
+	void Update () {
+		
+	}
+}
